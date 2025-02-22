4ae3af09184b074ec9fd4634e4cbb56dbe191ae3
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-02-22 20:08:40 -0330

    Created HttpClient class for making HTTP requests - GET, POST, PUT, DELETE

--- src/main/java/com/socialpetwork/http/client/PostClient.java
+++ src/main/java/com/socialpetwork/http/client/PostClient.java
@@ -1,4 +1,32 @@
 package com.socialpetwork.http.client;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.socialpetwork.domain.PostDTO;
+import com.socialpetwork.util.HttpClient;
+import com.socialpetwork.util.HttpResponse;
+
+import java.io.IOException;
+import java.util.List;
+
 public class PostClient {
+
+    private final String BASE_URL = "http://localhost:8080/posts";
+    private final HttpClient httpClient;
+    private final ObjectMapper objectMapper;
+
+    public PostClient() {
+        httpClient = new HttpClient();
+        objectMapper = new ObjectMapper();
+    }
+
+    public List<PostDTO> getAllPosts() {
+        String url = BASE_URL;
+        try {
+            HttpResponse response = httpClient.get(url);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
